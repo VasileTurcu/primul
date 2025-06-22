2226d861e1f31cd96a100869849b971eb7f480ac
VasileTurcu <vasioc45@gmail.com>
2025-06-22 20:06:28 +0200

    descrierea persoanei

--- lucru.py
+++ lucru.py
@@ -134,12 +134,18 @@ Exemplu: Pentru numele "Ana", vârsta 32 și orașul "București" rezultatul va 
 """
 
 # CODUL TĂU VINE MAI JOS:
+def task_10(nume,varsta,oras='optional'):
+    
+    
+    return  f'Numele:{nume}, Varsta:{varsta},Oras:{oras}'
+
+
 # print("Ex 10:")
 
-# nume = input("Dati un nume: ")
-# varsta = int(input("Dati varsta: "))
-# oras = input("Dati orasul (optional): ")
-# print(task_10(nume, varsta, oras))
+nume = input("Dati un nume: ")
+varsta = int(input("Dati varsta: "))
+oras = input("Dati orasul (optional): ")
+print(task_10(nume, varsta, oras))
 # CODUL TĂU VINE MAI SUS:
 
 """
